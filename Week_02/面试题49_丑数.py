a5fe363d291de9fd2e377b876e219dfa1988a982
zuishuailxy <zuishuailxy@gmail.com>
2020-06-03 21:21:25 +0800

    week02

---
+++ Week_02/面试题49_丑数.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        # 动态规划，TC:O(n), SC:O(n)
+        dp, a, b, c = [1] * n, 0, 0, 0
+        for i in range(1, n):
+            n2, n3, n5 = dp[a] * 2, dp[b] * 3, dp[c] * 5
+            dp[i] = min(n2, n3, n5)
+            if dp[i] == n2: a += 1
+            if dp[i] == n3: b += 1
+            if dp[i] == n5: c += 1
+        
+        return dp[-1]
